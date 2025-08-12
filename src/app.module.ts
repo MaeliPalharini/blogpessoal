061b4273db7d07e29cbabf18855fd4b3ce787261
Maeli <maeli.palharini@hotmail.com>
2025-08-12 00:37:24 -0300

    style: organizar configuração do AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
@@ -8,32 +8,29 @@ import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
 
-let isProd = process.env.NODE_ENV === 'production';
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
 
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService): TypeOrmModuleOptions => ({
-        type: 'postgres',
-        url: config.get<string>('DATABASE_URL'),
-        autoLoadEntities: true,
-        synchronize: config.get('DB_SYNC') === 'true',
-        logging: false,
-        ssl: isProd ? true : false,
-        extra: isProd ? { ssl: { rejectUnauthorized: false } } : undefined,
-      }),
+      useFactory: (config: ConfigService) => {
+        const isProd = config.get<string>('NODE_ENV') === 'production';
+        return {
+          type: 'postgres',
+          url: config.get<string>('DATABASE_URL'),
+          autoLoadEntities: true,
+          synchronize: config.get<string>('DB_SYNC') === 'true',
+          logging: !isProd,
+        };
+      },
     }),
 
+    AuthModule,
     PostagemModule,
     TemaModule,
     UsuarioModule,
-    AuthModule,
   ],
   controllers: [AppController],
-  providers: [],
 })
 export class AppModule {}
