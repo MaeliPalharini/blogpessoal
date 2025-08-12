a1e3fc7e81295795811f1d03495c323ceb91d471
Maeli <maeli.palharini@hotmail.com>
2025-08-12 04:50:44 -0300

    🐛 fix: arrumar login através do swagger

--- src/main.ts
+++ src/main.ts
@@ -21,9 +21,20 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:4000',
+      'https://blogpessoal-rc1l.onrender.com'
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
 
   const port = Number(process.env.PORT) || 4000;
   await app.listen(port);
