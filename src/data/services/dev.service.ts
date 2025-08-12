d22b36d5094b2a13c2cb869cf0048ca723d7921c
Maeli <maeli.palharini@hotmail.com>
2025-08-12 03:47:30 -0300

    🐛 fix: arrumar o devService para funcionar com mysql local

--- src/data/services/dev.service.ts
+++ src/data/services/dev.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { Usuario } from '../../usuario/entities/usuario.entity';
-import { Tema } from '../../tema/entities/tema.entity';
 import { Postagem } from '../../postagem/entities/postagem.entity';
+import { Tema } from '../../tema/entities/tema.entity';
+import { Usuario } from '../../usuario/entities/usuario.entity';
 
 @Injectable()
 export class DevService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      logging: false,
-      dropSchema: false,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Postagem, Tema, Usuario],
       synchronize: true,
-      autoLoadEntities: true,
-      entities: [Usuario, Tema, Postagem]
     };
   }
 }
