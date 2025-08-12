84f8f18f1d38442ea1d150f105a354ec09a6b99c
Maeli <maeli.palharini@hotmail.com>
2025-08-12 01:56:18 -0300

    ✅ fix: ajustar ProdService com as entidades

--- src/data/services/prod.services.ts
+++ src/data/services/prod.services.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Usuario } from '../../usuario/entities/usuario.entity';
+import { Tema } from '../../tema/entities/tema.entity';
+import { Postagem } from '../../postagem/entities/postagem.entity';
 
 @Injectable()
 export class ProdService implements TypeOrmOptionsFactory {
@@ -14,6 +17,7 @@ export class ProdService implements TypeOrmOptionsFactory {
       },
       synchronize: true,
       autoLoadEntities: true,
+      entities: [Usuario, Tema, Postagem]
     };
   }
 }
